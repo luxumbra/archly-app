ddafdee334e8a9948de6414fbbe6f39e5bde0e93
luxumbra <me@luxumbra.dev>
2025-09-10 16:37:38 +0100

    remove netlify config as we not using it anymore.

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "static",
-    adapter: node({
+  adapter: node({
     mode: "standalone",
   }),
   site: "https://yore.earth",
